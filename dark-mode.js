d3edeb7f7186ba4f026065430bf9a02a0f701a98
Hayfron24 <96965280+Hayfron24@users.noreply.github.com>
2023-10-06 16:11:23 +0000

    mobile and table media queris completed

---
+++ dark-mode.js
@@ -0,0 +1,6 @@
+const darkMode = localStorage.getItem('dark-mode');
+
+function switcher(){
+    const darkModeBtn = document.getElementById('dark-mode-btn').src = 'switch-dark.svg';
+    console.log('darkModeBtn');
+};
