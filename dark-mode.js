a0a0285f9460407626027d52e725470f11a63e01
Hayfron24 <96965280+Hayfron24@users.noreply.github.com>
2023-10-09 13:27:24 +0000

    project done, toggle works, action saves in localStorage

--- dark-mode.js
+++ dark-mode.js
@@ -1,19 +1,43 @@
-const darkMode = localStorage.getItem('dark-mode');
+const darkMode = localStorage.getItem('theme');
 
 const darkModeToggle = document.getElementById('darkModeToggle');
 const image = document.getElementById('image');
-
 const lightModeImg = 'design/switch.svg';
 const darkModeImg = 'design/switch-dark.svg'
+const darkModeElements = document.querySelectorAll('body, header, .followers-count, .single-overview');
 
+if(darkMode === 'dark-mode'){
+    applyDarkMode();
+}
 
 darkModeToggle.addEventListener('click', ()=> {
-      // Toggle between initial and alternate images
-  if (image.src.endsWith (lightModeImg)) {
-    image.src = darkModeImg;
-  } else {
-    image.src = lightModeImg;
-  }
+      // Toggle between initial and alternate image
+      if (image.src.endsWith (lightModeImg)) {
+          image.src = darkModeImg;
+          
+        } else {
+            image.src = lightModeImg;
+           
+        }
 
-});
+        darkModeElements.forEach(element => {
+          element.classList.toggle('dark-mode');
+        });
+        
+        if (image.src.endsWith(darkModeImg)){
+            localStorage.setItem('theme', 'dark-mode');
+        }else{
+            localStorage.setItem('theme', 'light-mode');
+        }
+
+        
+    });
+    function applyDarkMode(){
+        image.src = darkModeImg;
+        darkModeElements.forEach(element =>{
+            element.classList.toggle('dark-mode')
+        });
+    };
   
+
+
