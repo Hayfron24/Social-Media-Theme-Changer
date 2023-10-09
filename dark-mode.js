4c09d3001fb26cf2719846dc090ae2cd6ea0e94c
Hayfron24 <96965280+Hayfron24@users.noreply.github.com>
2023-10-09 01:51:26 +0000

    button toggle done

--- dark-mode.js
+++ dark-mode.js
@@ -1,6 +1,19 @@
 const darkMode = localStorage.getItem('dark-mode');
 
-function switcher(){
-    const darkModeBtn = document.getElementById('dark-mode-btn').src = 'switch-dark.svg';
-    console.log('darkModeBtn');
-};
+const darkModeToggle = document.getElementById('darkModeToggle');
+const image = document.getElementById('image');
+
+const lightModeImg = 'design/switch.svg';
+const darkModeImg = 'design/switch-dark.svg'
+
+
+darkModeToggle.addEventListener('click', ()=> {
+      // Toggle between initial and alternate images
+  if (image.src.endsWith (lightModeImg)) {
+    image.src = darkModeImg;
+  } else {
+    image.src = lightModeImg;
+  }
+
+});
+  
